51e53264621ecabda9eb1d6758acd9cdc9475f9c
MSDN-SPECIAL\Administrator <jsw1767@gmail.com>
2023-11-11 15:36:55 +0900

    feat(christmas): 총 혜택 금액에 따른 이벤트 배지 증정 기능
    세부 내용:
    - 혜택 금액이 5,000원 미만이면 배지 미부여
    - 혜택 금액이 5,000원 이상 10,000원 미만이면 별 배지 부여
    - 혜택 금액이 10,000원 이상 20,000원 미만이면 트리 배지 부여
    - 혜택 금액이 20,000원 이상이면 산타 배지 부여

---
+++ src/test/java/christmas/domain/BadgeTest.java
@@ -0,0 +1,66 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.constants.EventBadge;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class BadgeTest {
+    @DisplayName("총 혜택 금액이 2만원 이상이면 산타 배지를 증정한다.")
+    @ValueSource(ints = {20_000, 20_001})
+    @ParameterizedTest
+    void presentSantaBadge_WhenBenefitAmountIsInRequiredRange(int benefitAmount) {
+        // Given
+        Badge badge = new Badge();
+
+        // When
+        String badgeType = badge.checkForBadge(benefitAmount);
+
+        // Then
+        assertThat(badgeType).isEqualTo(EventBadge.SANTA.getBadge());
+    }
+
+    @DisplayName("총 혜택 금액이 1만원 이상, 2만원 미만이면 트리 배지를 증정한다.")
+    @ValueSource(ints = {10_000, 10_001, 19_999})
+    @ParameterizedTest
+    void presentTreeBadge_WhenBenefitAmountIsInRequiredRange(int benefitAmount) {
+        // Given
+        Badge badge = new Badge();
+
+        // When
+        String badgeType = badge.checkForBadge(benefitAmount);
+
+        // Then
+        assertThat(badgeType).isEqualTo(EventBadge.TREE.getBadge());
+    }
+
+    @DisplayName("총 혜택 금액이 5천원 이상, 1만원 미만이면 별 배지를 증정한다.")
+    @ValueSource(ints = {5_000, 5_001, 9_999})
+    @ParameterizedTest
+    void presentStarBadge_WhenBenefitAmountIsInRequiredRange(int benefitAmount) {
+        // Given
+        Badge badge = new Badge();
+
+        // When
+        String badgeType = badge.checkForBadge(benefitAmount);
+
+        // Then
+        assertThat(badgeType).isEqualTo(EventBadge.STAR.getBadge());
+    }
+
+    @DisplayName("총 혜택 금액이 5천원 미만이면 배지를 증정하지 않는다.")
+    @ValueSource(ints = {0, 1, 4_999})
+    @ParameterizedTest
+    void presentNothing_WhenBenefitAmountIsLessThanMinimumRequiredAmount(int benefitAmount) {
+        // Given
+        Badge badge = new Badge();
+
+        // When
+        String badgeType = badge.checkForBadge(benefitAmount);
+
+        // Then
+        assertThat(badgeType).isEqualTo(EventBadge.NOTHING.getBadge());
+    }
+}
